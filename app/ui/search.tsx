b33b9ecb52368404aa7acf8932bb4f2844e096fa
gabriel <innermond@yahoo.com>
2026-03-11 21:57:55 +0200

    add/update/del

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,6 +5,11 @@ import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const {replace} = useRouter();
+
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', 1);
@@ -15,10 +20,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 500);
-
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const {replace} = useRouter();
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
